a0b67d9a26f2e5558fa023d8004c15249eb89304
codesteller <pallab.maji@gahanai.com>
2024-10-03 14:05:07 +0530

    updated launcher

--- launch_container.sh
+++ launch_container.sh
@@ -5,21 +5,21 @@ XAUTH=/tmp/.docker.xauth
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-mkdir -p jetpack_home/nvidia
-mkdir -p jetpack_home/Downloads
-JETPACK_HOME=$(realpath ./jetpack_home)
+mkdir -p nvdrive_home/nvidia
+mkdir -p nvdrive_home/Downloads
+nvdrive_HOME=$(realpath ./nvdrive_home)
 
 docker run --privileged --rm -it \
            --volume=$XSOCK:$XSOCK:rw \
            --volume=$XAUTH:$XAUTH:rw \
            --volume=/dev:/dev:rw \
-           --volume="$JETPACK_HOME/nvidia":/home/jetpack/nvidia:rw \
-           --volume="$JETPACK_HOME/Downloads":/home/jetpack/Downloads:rw \
+           --volume="$nvdrive_HOME/nvidia":/home/nvdrive/nvidia:rw \
+           --volume="$nvdrive_HOME/Downloads":/home/nvdrive/Downloads:rw \
            --shm-size=1gb \
            --env="XAUTHORITY=${XAUTH}" \
            --env="DISPLAY=${DISPLAY}" \
            --env=TERM=xterm-256color \
            --env=QT_X11_NO_MITSHM=1 \
            --net=host \
-           -u "jetpack"  \
+           -u "nvdrive"  \
            nvdriveos_20.04:latest
